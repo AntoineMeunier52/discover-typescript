2eaf9b9373760bb34e922b5fd0cbb067e9e55801
AntoineMeunier52 <meunier.antoine52@gmail.com>
2021-02-17 13:31:48 +0100

    custom type

---
+++ dist/customTypes.js
@@ -0,0 +1,6 @@
+const car = {
+    brand: "Fiat",
+    carName: "Multiplat",
+    age: 15,
+    option: ["vitres electriques", "sieges chauffants"]
+};
--- dist/function.js
+++ dist/function.js
@@ -2,7 +2,6 @@ function addition(x, y) {
     return x + y;
 }
 function showResult() {
-    console.log(3);
     console.log(addition(2, 8));
 }
 //LE VOID SIGNIFIE QUE LA FUNCTION NE RETOURNE RIEN LE CONTRAIRE DE ANY
