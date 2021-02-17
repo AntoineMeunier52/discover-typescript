694e81acd3a47b1c17e194f4e9fdfee3588def80
AntoineMeunier52 <meunier.antoine52@gmail.com>
2021-02-17 13:30:33 +0100

    custom type

---
+++ src/customTypes.ts
@@ -0,0 +1,9 @@
+type Car = { brand: string; carName: string; age: number; option: string[]}
+
+
+const car: Car = {
+    brand: "Fiat",
+    carName: "Multiplat",
+    age: 15,
+    option: ["vitres electriques", "sieges chauffants"]
+}
